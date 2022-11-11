e30d524829b60bc2c78f5ab071a19d7264cd8bae
Yansantos66 <104377988+Yansantos66@users.noreply.github.com>
2022-11-11 11:26:21 -0300

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-import serverless_wsgi
+import serverless_wsgi 
 import requests
 from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_s3 import FlaskS3 
 from datetime import datetime, timedelta
-import json
+import boto3
 
-with open('dados.json') as jsonfile:
-    clientes = json.load(jsonfile)
-
-app = Flask(__name__,static_url_path='/static', static_folder='static')
+app = Flask(__name__, static_folder='static')
 app.config["SECRET_KEY"] = "key_secret"
 app.config['FLASKS3_BUCKET_NAME'] = 'bucket-storage' 
 s3 = FlaskS3() 
 s3.init_app(app)
+
+@app.context_processor
+def inject_static_url():
+    """
+    Inject the variable 'static_url' into the templates. Grab it from
+    the environment variable STATIC_URL, or use the default.
+
+    Template variable will always have a trailing slash.
+
+    """
+    return dict(
+        static_url='https://bucket-storage.s3.sa-east-1.amazonaws.com/'
+    )
 
 @app.route("/")
 def homepage():
@@ -31,12 +41,12 @@ def cadastro():
         data_minima = datetime.now() - timedelta(days=18 * 365)
         idade_cliente = datetime.strptime(datcliente, "%Y-%m-%d")
         idade_permitida = idade_cliente < data_minima
-
         for cliente in clientes:
             if cliente["cpf"] == cpfcliente or cliente["celular"] == celcliente:
-                flash("Cliente já cadastrado")
+                flash(f"Cliente já cadastrado!")
                 return render_template("conta.html")
-                #if idade_permitida:
+        #if idade_permitida: 
+            
 def put_user(ncliente, cpfcliente, datcliente, celcliente, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource(
@@ -52,18 +62,17 @@ def put_user(ncliente, cpfcliente, datcliente, celcliente, dynamodb=None):
            }
         )  
         return response
-            flash("Cliente cadastrado com sucesso. Agora você faz parte do time PagBank PagSeguro!")
-        else:
-            flash("Analisamos que você ainda não tem idade mínima permitida, e por isso não podemos prosseguir com seu cadastro.")
+            
+        flash("Cliente cadastrado com sucesso! Agora você faz parte do time PagBank PagSeguro!")
+    else:
+        flash("Analisamos que você ainda não tem idade mínima permitida, e por isso não podemos prosseguir com seu cadastro. ")
     return render_template("conta.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
-#if __name__ == "__main__":
- #   app.run(debug=True)   
-  #  clientes = [
-   #     {"nome": "", "cpf": "", "data": "", "celular": ""},
-    #    {"nome": "", "cpf": "", "data": "", "celular": ""},
-    #]
 def handler(event, context):
     return serverless_wsgi.handle_request(app, event, context)
+#if __name__ == "__main__":
+#    app.run(debug=True)
+ #   clientes = [
+  #      {"nome": "", "cpf": "", "data": "", "celular": ""},
+   #     {"nome": "", "cpf": "", "data": "", "celular": ""},
+    #]
